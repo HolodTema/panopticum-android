1f34b14c5d9ff7cf0cbd95fa1007c97a5bf1606e
artem <art.holodilov@gmail.com>
2026-07-09 19:37:19 +0300

    Started configuring Hilt to multi-module project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -58,4 +60,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Hilt for DI
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.android.compiler)
 }
